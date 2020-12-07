f76945014b8c542fa54a17e62e14519bd1e343a8
Chris Robinson <stoffle@users.noreply.github.com>
2020-12-07 13:51:00 +0000

    added reltationship stuff, and new full dataset/configs

---
+++ scripts/intervene_run_relationship_20201204.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+intervene real --data ../data/NIBRS_relationships_202012041558.csv --output ../output/intervene_run_relationship --constraints config_files/relationship_constraints.yml --interventions config_files/relationship_intervention.yml
