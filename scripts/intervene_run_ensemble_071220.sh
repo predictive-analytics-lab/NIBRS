eac526eef393855e975823bde63f2a54c3ff03c2
bradley-butcher <bradleydpbutcher@gmail.com>
2020-12-07 14:01:05 +0000

    add bs_prop into script

--- scripts/intervene_run_ensemble_071220.sh
+++ scripts/intervene_run_ensemble_071220.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-intervene real --data ../data/NIBRS_full_202012071235.csv --output ../output/ensemble_run_071220 --constraints config_files/full_constraints.yml --interventions config_files/full_intervention.yml --ensemble
+intervene real --data ../data/NIBRS_full_202012071235.csv --output ../output/ensemble_run_071220 --constraints config_files/full_constraints.yml --interventions config_files/full_intervention.yml --ensemble --bootstrap_prop 0.5
