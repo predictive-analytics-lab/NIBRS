5b4295c2762f59772c76fcf769343dd568bacf5f
bradley-butcher <bradleydpbutcher@gmail.com>
2020-12-08 15:53:40 +0000

    change firearm-stranger to stranger

--- scripts/intervene_runs/stranger_firearm_ensemble_081220.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-intervene real --data ../../data/NIBRS_full_202012071235.csv --output ../../output/stranger_firearm --constraints ../config_files/constraints/full.yml --interventions ../config_files/interventions/full_stranger_firearm.yml --ensemble --bootstrap_prop 0.5
