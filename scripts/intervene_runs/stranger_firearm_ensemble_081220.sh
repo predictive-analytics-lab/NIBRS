ad18e54d7ec663e435dcf832783a5f7b62e4ab41
bradley-butcher <bradleydpbutcher@gmail.com>
2020-12-08 14:41:42 +0000

    change output dir of stranger firearm

--- scripts/intervene_runs/stranger_firearm_ensemble_081220.sh
+++ scripts/intervene_runs/stranger_firearm_ensemble_081220.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-intervene real --data ../../data/NIBRS_full_202012071235.csv --output ../../output/ensemble_run_071220 --constraints ../config_files/constraints/full.yml --interventions ../config_files/interventions/full_stranger_firearm.yml --ensemble --bootstrap_prop 0.5
+intervene real --data ../../data/NIBRS_full_202012071235.csv --output ../../output/stranger_firearm --constraints ../config_files/constraints/full.yml --interventions ../config_files/interventions/full_stranger_firearm.yml --ensemble --bootstrap_prop 0.5
