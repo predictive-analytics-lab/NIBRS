656bd8ff81a833ddf54aaea6f3433211ef880832
bradley-butcher <bradleydpbutcher@gmail.com>
2020-12-11 11:00:21 +0000

    fixed constraints

--- scripts/intervene_runs/full_ensemble_101220.sh
+++ scripts/intervene_runs/full_ensemble_101220.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-intervene real --data ../../data/NIBRS_full_202012081725.csv --output ../../output/full_ensemble --constraints ../config_files/constraints/full_20201210.yml --interventions ../config_files/interventions/full_20201208.yml --ensemble --bootstrap_prop 0.5
+intervene real --data ../../data/NIBRS_full_202012081725.csv --output ../../output/full_ensemble --constraints ../config_files/constraints/full_20201211.yml --interventions ../config_files/interventions/full_20201208.yml --ensemble --bootstrap_prop 0.5
