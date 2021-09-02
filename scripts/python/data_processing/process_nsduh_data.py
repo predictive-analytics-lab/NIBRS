831859d3ce74114d02efd73c863f933c7dc020a0
Stoffle <stoffle@users.noreply.github.com>
2021-09-02 16:05:14 +0100

    made nsduh processing script work for other drug types

--- scripts/python/data_processing/process_nsduh_data.py
+++ scripts/python/data_processing/process_nsduh_data.py
@@ -1,5 +1,6 @@
 """This python file loads the NSDUH dataset and formats it for the project needs."""
 
+from typing import Literal
 import pandas as pd
 from pathlib import Path
 import argparse
@@ -12,8 +13,9 @@ columns_to_use = [
     "NEWRACE2",
     "CATAG3",
     "IRSEX",
-    "MJDAY30A",
     "ANALWT_C",
+    "POVERTY3",
+    "COUTYP4"
 ]
 
 
@@ -54,6 +56,14 @@ urbancounty_dict = {
 }
 
 
+usage_column_dict = {
+    "cannabis": "MJDAY30A",
+    "cocaine": "COCUS30A",
+    "crack": "CRKUS30A",
+    "meth": "METHAM30N"
+}
+
+
 def usage(n):
     if n <= 30:
         return n
@@ -71,39 +81,41 @@ def binary_usage(n):
 ######### Data Loading ##########
 
 
-def join_years(years: str) -> pd.DataFrame:
+def join_years(years: str, drug: Literal["cannabis", "cocaine", "crack", "meth"]) -> pd.DataFrame:
     """
     Function which converts a string range of years to a list of year ints.
     Following this, each corresponding NSDUH year is loaded and merged with the
     NSDUH data.
     """
     if "-" not in years:
-        return load_and_process_nsduh(int(years))
+        return load_and_process_nsduh(int(years), drug)
     years = years.split("-")
     years = range(int(years[0]), int(years[1]) + 1)
     
     assert min(years) >= 2012 and max(years) <= 2019, "Invalid year range. Valid range is: 2012-2019."
+    assert drug in ["cannabis", "cocaine", "crack", "meth"], "Invalid drug type, must be one of [cannabis, cocaine, crack, meth]"
 
     # Load NSDUH data
-    nsduh_df = load_and_process_nsduh(years[0])
+    nsduh_df = load_and_process_nsduh(years[0], drug)
 
     # Load each year of NSDUH data
     for year in years[1:]:
-        nsduh_df = nsduh_df.append(load_and_process_nsduh(year), ignore_index=True)
+        nsduh_df = nsduh_df.append(load_and_process_nsduh(year, drug), ignore_index=True)
 
     return nsduh_df
 
-def load_and_process_nsduh(year: int) -> pd.DataFrame:
+def load_and_process_nsduh(year: int, drug: str) -> pd.DataFrame:
     # This is the NSDUH dataset. Only AGE/RACE/SEX/MJDAY30A are selected.
     nsduh_df = pd.read_csv(data_path / "NSDUH" / f"NSDUH_{year}_Tab.tsv",
-                        sep="\t", usecols=columns_to_use)
+                        sep="\t", usecols=columns_to_use+[usage_column_dict[drug]])
 
     nsduh_df.rename(columns={
         "NEWRACE2": "race",
         "CATAG3": "age",
         "IRSEX": "sex",
         "POVERTY3": "poverty",
-        "COUTYP4": "urbancounty"
+        "COUTYP4": "urbancounty",
+        usage_column_dict[drug]: "usage_30day"
     }, inplace=True)
 
     nsduh_df["race"] = nsduh_df.race.map(race_dict)
@@ -111,8 +123,8 @@ def load_and_process_nsduh(year: int) -> pd.DataFrame:
     nsduh_df["sex"] = nsduh_df.sex.map(sex_dict)
     nsduh_df["poverty"] = nsduh_df.poverty.map(poverty_dict)
     nsduh_df["urbancounty"] = nsduh_df.urbancounty.map(urbancounty_dict)
-    nsduh_df["MJBINARY"] = nsduh_df.MJDAY30A.map(binary_usage)
-    nsduh_df["MJDAY30A"] = nsduh_df.MJDAY30A.map(usage)
+    nsduh_df["usage_binary"] = nsduh_df["usage_30day"].map(binary_usage)
+    nsduh_df["usage_30day"] = nsduh_df["usage_30day"].map(usage)
     nsduh_df = nsduh_df[nsduh_df.race != "other/mixed"]
     nsduh_df["year"] = year
 
@@ -122,18 +134,14 @@ def load_and_process_nsduh(year: int) -> pd.DataFrame:
 if __name__ == "__main__":
     parser=argparse.ArgumentParser()
 
-    parser.add_argument("--year", help="year, or year range.")
+    parser.add_argument("--year", help="year, or year range.", default="2019")
+    parser.add_argument("--drug", help="drug type", default="cannabis")
 
     args=parser.parse_args()
 
-    if not args.year:
-        print("No year specified. Defaulting to 2019")
-        df = join_years("2019")
-    else:
-        df = join_years(args.year)
+    df = join_years(args.year, args.drug)
 
-    year = args.year if args.year else "2019"
-    df.to_csv(data_path / "NSDUH" / f"nsduh_processed_{year}.csv")
+    df.to_csv(data_path / "NSDUH" / f"nsduh_processed_{args.drug}_{args.year}.csv")
 
 
 
