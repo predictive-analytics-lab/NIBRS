122f29eb891c4cfff78dd86dd73f8056226ffa0f
ricfog <rfogliat@andrew.cmu.edu>
2021-09-23 20:03:45 +0200

    fixed table with summary stats for nibrs

--- scripts/R/get_stats_on_nibrs.R
+++ scripts/R/get_stats_on_nibrs.R
@@ -27,33 +27,66 @@ df$all_locations_2 <- all_locations %>% purrr::map(~ .x[[2]]) %>% unlist() %>% t
 get_incidents_info <- function(x){
   
   all_incidents <- nrow(x)
+  xsub <- x %>% filter(arrest_type_name != 'No Arrest')
+  arrests <- nrow(xsub)
   
   tb <- tribble(
-    ~term, ~value,
-    "% drug only incidents",  nrow(x %>% filter(!other_offense))/all_incidents,
-    "% possessing", nrow(x %>% filter(criminal_act == 'possessing'))/all_incidents,
-    "% consuming", nrow(x %>% filter(criminal_act == 'consuming'))/all_incidents,
+    ~term, ~value, ~value_arrest,
+    "% drug only incidents",  
+    nrow(x %>% filter(!other_offense))/all_incidents, 
+    nrow(xsub %>% filter(!other_offense))/arrests,
+    
+    "% possessing", 
+    nrow(x %>% filter(criminal_act == 'possessing'))/all_incidents,
+    nrow(xsub %>% filter(criminal_act == 'possessing'))/arrests,
+    
+    "% consuming", 
+    nrow(x %>% filter(criminal_act == 'consuming'))/all_incidents,
+    nrow(xsub %>% filter(criminal_act == 'consuming'))/arrests,
+    
     "% buying", nrow(x %>% filter(criminal_act == 'buying'))/all_incidents,
+    nrow(xsub %>% filter(criminal_act == 'buying'))/arrests,
+    
     "% selling", nrow(x %>% filter(criminal_act == 'selling'))/all_incidents,
+    nrow(xsub %>% filter(criminal_act == 'selling'))/arrests,
+    
     "% distributing", nrow(x %>% filter(criminal_act == 'distributing'))/all_incidents,
+    nrow(xsub %>% filter(criminal_act == 'distributing'))/arrests,
+    
     '% other drugs present', nrow(x %>% filter(other_drugs_count > 0))/all_incidents,
+    nrow(xsub %>% filter(other_drugs_count > 0))/arrests,
+    
     "% single offender", nrow(x %>% filter(offender_count == 1))/all_incidents,
+    nrow(xsub %>% filter(offender_count == 1))/arrests,
+    
     "% residence", sum(grepl('residence', pull(x, all_locations_1)) + grepl('residence', pull(x, all_locations_2))>0)/all_incidents,
+    sum(grepl('residence', pull(xsub, all_locations_1)) + grepl('residence', pull(xsub, all_locations_2))>0)/arrests,
+    
     "% hotel", sum(grepl('hotel', pull(x, all_locations_1)) + grepl('hotel', pull(x, all_locations_2))>0)/all_incidents,
+    sum(grepl('hotel', pull(xsub, all_locations_1)) + grepl('hotel', pull(xsub, all_locations_2))>0)/arrests,
+    
     "% highway/road", sum(grepl('road', pull(x, all_locations_1)) + grepl('road', pull(x, all_locations_2))>0)/all_incidents,
+    sum(grepl('road', pull(xsub, all_locations_1)) + grepl('road', pull(xsub, all_locations_2))>0)/arrests,
+    
     "% parking lot/garage", sum(grepl('parking', pull(x, all_locations_1)) + grepl('parking', pull(x, all_locations_2))>0)/all_incidents,
+    sum(grepl('parking', pull(xsub, all_locations_1)) + grepl('parking', pull(xsub, all_locations_2))>0)/arrests,
+    
     '% during day (6-20)', nrow(x %>% filter(incident_hour > 5 & incident_hour < 21))/nrow(x %>% filter(!is.na(incident_hour))),
+    nrow(xsub %>% filter(incident_hour > 5 & incident_hour < 21))/nrow(xsub %>% filter(!is.na(incident_hour))),
+    
     '% no arrest', nrow(x %>% filter(arrest_type_name == "No Arrest"))/all_incidents,
+    NA,
+    
     '% arrest: custody', nrow(x %>% filter(arrest_type_name == 'Taken INTO Custody'))/all_incidents,
-    '% arrest: on view', nrow(x %>% filter(arrest_type_name == 'On View'))/all_incidents,
-    '% arrest: summoned/cited', nrow(x %>% filter(arrest_type_name == 'Summoned / Cited'))/all_incidents
+    NA,
+    '% arrest: on view', nrow(x %>% filter(arrest_type_name == 'On View'))/all_incidents,NA,
+    '% arrest: summoned/cited', nrow(x %>% filter(arrest_type_name == 'Summoned / Cited'))/all_incidents, NA
   )
   tb <- tb %>%
-    mutate(value_se = sqrt(value * (1-value)/nrow(x)))
+    mutate(value_se = sqrt(value * (1-value)/all_incidents),
+           value_arrest_se = sqrt(value_arrest * (1-value_arrest)/arrests))
   return(tb)
 }
-
-df_inc <- get_incidents_info(df)
 
 
 df_list <- list(
@@ -71,21 +104,35 @@ df_list <- list(
 df_stats <- df_list %>%
   purrr::map(~ get_incidents_info(.x))
 
+df_stats_drugs <- df_list %>%
+  purrr::map(~ get_incidents_info(.x %>% filter(!other_offense)))
+
+
+
+
 df_stats_joined <- df_stats %>%
- # purrr::map(~ .x %>%
-               #mutate(value = glue('{round(value*100)}% ({round(value_se*100)})'))
-#               )
-  purrr::map(~ .x %>% mutate(value = glue('{round(value*100)}%')) %>%
-               select(-value_se)) %>%
+  purrr::map(~ .x %>% 
+               mutate(is_numeric_arrest = ifelse(!is.na(as.numeric(value_arrest)), TRUE, FALSE)) %>%
+               mutate(value = glue('{round(value*100)}%{ifelse(is_numeric_arrest, paste0(" [",round(value_arrest*100), "%]"), "")}')) %>%
+               select(term ,value)) %>%
   reduce(left_join, by = 'term')
 
+df_stats_drugs_joined <- df_stats_drugs %>%
+  purrr::map(~ .x %>% 
+               mutate(is_numeric_arrest = ifelse(!is.na(as.numeric(value_arrest)), TRUE, FALSE)) %>%
+               mutate(value = glue('{round(value*100)}%{ifelse(is_numeric_arrest, paste0(" [",round(value_arrest*100), "%]"), "")}')) %>%
+               select(term ,value)) %>%
+  reduce(left_join, by = 'term')
+
+
 n_incidents <- df_list %>% purrr::map(~ nrow(.x)) %>% unlist()
+n_incidents_drugs <- df_list %>% purrr::map(~ nrow(.x %>% filter(!other_offense))) %>% unlist()
 
 
 print(rbind(
   c('# incidents', n_incidents),
   df_stats_joined
-) %>% xtable(.,  align = rep('c', 11)),
+) %>% xtable(.,  align = rep('c', 10)),
          include.rownames=FALSE)  
   
 
